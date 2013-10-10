3afdce7a62c23dce64b40f9aa3c2787c09877faa
DjebbZ <khalid.jebbari@gmail.com>
2013-10-10 17:35:44 +0200

    Basic code structure with first dumb test

---
+++ test/spec.js
@@ -0,0 +1,13 @@
+assert = require('assert');
+
+describe('Array', function() {
+
+  describe('.length', function() {
+
+    it('should return the length of the array', function() {
+        assert.equal(0, [].length);
+    });
+
+  });
+
+});
